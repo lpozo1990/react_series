b000a3b47ab0f6a5bfa867d30171e1529c7a5f97
lpozo1990 <lpozo1990@gmail.com>
2022-05-04 16:43:09 -0400

    Mock the server axios call

--- tutorial/src/Components/Parent.tsx
+++ tutorial/src/Components/Parent.tsx
@@ -1,14 +1,40 @@
 import Child from './Child';
 import '../App.css';
 import { useState } from 'react';
+import axios from 'axios';
 
-function Parent() {
+  interface Post {
+    id: number;
+    userId: number;
+    title: string;
+    body: string;
+  }
+
+function Parent(props:any) {
   const [counter, setCounter] = useState(0);
   const sendDataToParent = () => setCounter(counter+1);
+  const [list, setList] = useState([]);
+  const base_url = props.url ? props.url : "https://jsonplaceholder.typicode.com/posts";
+  const fetchData = async () => {
+    const response = await  axios.get(base_url);
+    setList(response.data)
+}
+
+
   return (
     <div className="App">
       <Child sendDataToParent={sendDataToParent} />
       <span data-testid="counter-text">{counter}</span>
+      <br />
+      <button data-testid="fetch-btn" onClick={fetchData}>Fetch data</button>
+      <div data-testid='fetch-result'>
+        {list.map((item:Post) => (
+          <div key={item.id}>
+            <h3 data-testid='post-title'>{item.title}</h3>
+            <p>{item.body}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- tutorial/src/tests/Parent.test.tsx
+++ tutorial/src/tests/Parent.test.tsx
@@ -1,10 +1,36 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node'
 import Child from '../Components/Child';
 import Parent from '../Components/Parent';
+
+
+const server = setupServer(
+  rest.get('https://jsonplaceholder.typicode.com/posts', (req, res, ctx) => {
+    return res(ctx.json([
+      {
+        userId: 1,
+        id: 1,
+        title: "dummy response",
+        body: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
+      }]))
+  }),
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
 
 test('We expect the text of the child component to be in the document', () => {
   render(<Parent />);
   render(<Child name="Lucas" />);
-  const child_component_content = screen.getByText(/Hello, Lucas/i);  
+  const child_component_content = screen.getByText(/Hello, Lucas/i);
   expect(child_component_content).toBeInTheDocument();
 });
+
+test('The response is expected to be successful', async () => {
+  render(<Parent />);
+  fireEvent.click(screen.getByText('Fetch data'))
+  await waitFor(() => screen.getAllByTestId('post-title'));
+  expect(screen.getAllByTestId('post-title')[0].textContent).toBe('dummy response')
+})
