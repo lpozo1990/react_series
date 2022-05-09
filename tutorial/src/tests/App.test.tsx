3f809dfb62fe9d65f7c1ae3ecd5fcf0445b07d17
lpozo1990 <lpozo1990@gmail.com>
2022-05-09 18:39:05 -0400

    Test green

--- tutorial/src/tests/App.test.tsx
+++ tutorial/src/tests/App.test.tsx
@@ -1,11 +1,11 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import App from '../App';
 import { createMemoryHistory } from 'history';
 import store from '../store';
-import { BrowserRouter, MemoryRouter, Router } from 'react-router-dom';
-import Parent from '../Components/Parent';
 import userEvent from '@testing-library/user-event';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import Page2 from '../Components/Page2';
 
 test('We expect the text of the child component to be in the document', () => {
   render(
@@ -19,17 +19,20 @@ test('We expect the text of the child component to be in the document', () => {
   expect(child_component_content).toBeInTheDocument();
 });
 
-test('If I click the `go to page 2` link, I will see the content of page 2',  async () => {
+test('If I click the `go to page 2` link, I will see the content of page 2', async () => {
   const history = createMemoryHistory();
   render(
     <Provider store={store}>
-      <BrowserRouter >
-        <App />
-      </BrowserRouter>
+      <MemoryRouter initialEntries={["/"]}>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/page-2" element={<Page2 />} />
+        </Routes>
+      </MemoryRouter>
     </Provider>
   );
   const link_to_page_2 = screen.getByText('Go to page2');
   const user = userEvent
   await user.click(link_to_page_2)
-  expect(window.location.pathname).toBe('/page-2');
+  screen.getByText('you are on page 2')
 });
